0b9839bddc0a0524a105facdfb2976e986294c79
giDai7ja <36735901+giDai7ja@users.noreply.github.com>
2018-09-20 09:40:46 +0300

    Revert "test"
    This reverts commit a31b3d3385cae3458ef244badd87ee4fad79128c.

--- FW/FW.ino
+++ FW/FW.ino
@@ -1,52 +1,49 @@
 #include <SPI.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
 #include <EEPROM.h>
 
-#define ONE_WIRE_BUS 10
-
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
+//#define
 
 unsigned int L;
-unsigned int LL;
-byte addr[8];
-byte data[12];
-int celsius;
-byte i;
-
-//OneWire ds(10);
 
 // ----- Инициализация
 void setup() {
-  Serial.begin(19200);
+  // Serial.begin(19200);
   pinMode(9, OUTPUT); // Chip Select
   digitalWrite(9, HIGH);
+//  pinMode(11, OUTPUT);  // Data
+//  digitalWrite(11, LOW);
+//  pinMode(13, OUTPUT); // SCK
+//  digitalWrite(13, LOW);
+
+  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE0));
+  SPI.begin();
+
+//  delay(1000);
 
   pinMode(A0, INPUT); // Датчик освещённости
   pinMode(A3, OUTPUT);
   digitalWrite(A3, HIGH);
-  delay(500);
+    delay(500);
   pinMode(A4, OUTPUT);
   digitalWrite(A4, HIGH);
-  delay(500);
+    delay(500);
   pinMode(A5, OUTPUT);
   digitalWrite(A5, HIGH);
-  delay(500);
-  //  pinMode(A2, OUTPUT);
-  //  digitalWrite(A2, HIGH);
-  delay(500);
+    delay(500);
+  pinMode(A6, OUTPUT);
+  digitalWrite(A6, HIGH);
+    delay(500);
 
-  Serial.println("0");
-
-  /*  Send7219(0x01, 0x00);
-    Send7219(0x02, 0x0D);
-    Send7219(0x03, 0x0B);
-    Send7219(0x04, 0x0C);
-  */
-  //  ds.search(addr);
-  sensors.begin();
-  Serial.println("1110");
+  Send7219(0x00, 0x00);
+  Send7219(0xFF, 0x00);
+  Send7219(0x09, 0xFF);
+  Send7219(0x0A, 0xFF);
+  Send7219(0x0B, 0x03);
+  Send7219(0x0C, 0xFF);
+  Send7219(0x01, 0x00);
+  Send7219(0x02, 0x0D);
+  Send7219(0x03, 0x0B);
+  Send7219(0x04, 0x0C);
 
 }
 // ----- Конец инициализации
@@ -55,27 +52,16 @@ void setup() {
 
 // ----- Главный цикл
 void loop() {
-  /*
-    Serial.print("11");
-    LL = analogRead(A0);
-    if ( LL < 100 ) LL = 0;
-    else LL = (LL - 100) / 64;
-    Serial.print("12");
-    if (LL > 15) LL = 15;
-    Serial.print("13");
-    Send7219(0x0A, (byte)LL);
-  */
+L=analogRead(A0);
+if ( L < 100 ) L=0;
+else L=(L-100)/64;
 
-  Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-  // After we got the temperatures, we can print them here.
-  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
-  Serial.print("Temperature for the device 1 (index 0) is: ");
-  celsius = sensors.getTempCByIndex(0) * 10;
-  Serial.println(celsius/10);
+if (L>15) L=15;
 
-  SendTemp();
+Send7219(0x0A, L);
+
+
+delay(100);
 }
 // ----- Конец главного цикла
 
@@ -86,44 +72,3 @@ void Send7219(byte Addr, byte Data) {
   digitalWrite(9, HIGH);
 }
 
-void SendTemp() {
-  SPI.beginTransaction(SPISettings(16000000, MSBFIRST, SPI_MODE0));
-  SPI.begin();
-
-  Send7219(0x00, 0x00);
-  Send7219(0xFF, 0x00);
-  Send7219(0x09, 0xFF);
-  Send7219(0x0A, 0xFF);
-  Send7219(0x0B, 0x03);
-  Send7219(0x0C, 0xFF);
-
-  L = celsius % 10;
-  Serial.print("C=");
-  Serial.println(celsius);
-  Serial.print("L=");
-  Serial.println(L);
-  Send7219(0x01, L);
-  celsius = celsius / 10;
-  L = celsius % 10;
-  Serial.print("C=");
-  Serial.println(celsius);
-  Serial.print("L=");
-  Serial.println(L);
-  Send7219(0x02, L + 128);
-  celsius = celsius / 10;
-  L = celsius % 10;
-  Serial.print("C=");
-  Serial.println(celsius);
-  Serial.print("L=");
-  Serial.println(L);
-  Send7219(0x03, L);
-  celsius = celsius / 10;
-  L = celsius % 10;
-  Serial.print("C=");
-  Serial.println(celsius);
-  Serial.print("L=");
-  Serial.println(L);
-  Send7219(0x04, L);
-
-  SPI.end();
-}
